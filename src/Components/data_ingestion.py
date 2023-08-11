d9f65ccc7642b27a591173be6a1a61f796a3572d
Saravanan <saravanansd634@gmail.com>
2023-08-12 00:00:10 +0530

    Data Ingestion complete

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from src.exception import CustomException
-from logger import logging
+from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -9,8 +9,8 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join("artifact", "train.csv")
-    test_data_path: str=os.path.join("artifact", "test.csv")
+    train_data_path: str=os.path.join("artifacts", "train.csv")
+    test_data_path: str=os.path.join("artifacts", "test.csv")
     raw_data_path: str=os.path.join("artifacts",'data.csv')
 
 class DataIngestion:
@@ -20,10 +20,10 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('stud.csv')
+            df=pd.read_csv('notebook\stud.csv')
             logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False, header=True)
             logging.info("Train test split initiated")
